2091e623270ae60ce6925289229fe873f98e0ed5
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-04-17 19:04:02 -0300

    Rename JS class for better consistency

--- www/plugin.js
+++ www/plugin.js
@@ -2,10 +2,10 @@
 var exec = require('cordova/exec');
 
 var NATIVE_CLASS_NAME = "FluigLoginWrapper";
-var FluigLoginWrapper = {};
+var FluigSdkFlows = {};
 
-FluigLoginWrapper.start = function(success, error) {
+FluigSdkFlows.login = function(success, error) {
   exec(success, error, NATIVE_CLASS_NAME, "start", []);
 };
 
-module.exports = FluigLoginWrapper;
+module.exports = FluigSdkFlows;
