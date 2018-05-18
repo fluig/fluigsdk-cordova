53e69105c26bb83d3a6e997cc3e0390a5000420c
Gregory Sholl e Santos <gregory.santos@fluig.com>
2018-05-18 14:19:32 -0300

    Fix imports for Android code

--- src/android/sdk/fluig/com/cordova/FluigInformationWrapper.java
+++ src/android/sdk/fluig/com/cordova/FluigInformationWrapper.java
@@ -13,6 +13,7 @@ import sdk.fluig.com.datasource.model.core.User;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
